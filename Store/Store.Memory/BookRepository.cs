e6d37c7f7cac995c9c3132e03ace9ca8140b224e
Express01 <115688744+Express01@users.noreply.github.com>
2023-02-10 11:05:11 +0300

    Добавить реализацию репозитория книг в памяти

---
+++ Store/Store.Memory/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1,"Art of Programing"),
+            new Book(1,"Refactoring"),
+            new Book(1,"C Programing Language"),
+
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book=>book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
--- Store/Store/Book.cs
+++ Store/Store/Book.cs
@@ -1,7 +1,13 @@
 ﻿namespace Store
 {
-    public class Class1
+    public class Book
     {
-
+        public int Id { get; set; }
+        public string Title { get;}
+        public Book(int id, string title)
+        {
+            Title = title;
+            Id = id;
+        }
     }
 }
