ad359d12e5c7f952c43ec0679bab73d2cd30b4ea
Express01 <115688744+Express01@users.noreply.github.com>
2023-02-13 17:26:38 +0300

    Добавить страницу описание книги

--- Store/Store.Memory/BookRepository.cs
+++ Store/Store.Memory/BookRepository.cs
@@ -4,9 +4,9 @@
     {
         private readonly Book[] books = new[]
         {
-            new Book(1,"ISBN 12312-56371","D. Knuth","Art of Programing"),
-            new Book(1,"ISBN 12312-56372","M. Fowler","Refactoring"),
-            new Book(1,"ISBN 12312-56373","B. Kernighan, D. Rithie","C Programing Language"),
+            new Book(1,"ISBN 12312-56371","D. Knuth","Art of Programing","Cool bookghwdbhdfbhhdbf",7.19m),
+            new Book(2,"ISBN 12312-56372","M. Fowler","Refactoring","Cool book23dsfsffggsda",12.45m),
+            new Book(3,"ISBN 12312-56373","B. Kernighan, D. Rithie","C Programing Language","Cool bookggggggsdeefdfd",14.98m),
 
         };
 
@@ -19,5 +19,11 @@
         {
             return books.Where(book=>book.Author.Contains (query)||book.Title.Contains(query)).ToArray();
         }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book=>book.Id == id);
+        }
     }
 }
+
